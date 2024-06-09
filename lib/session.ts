eb8367c5b661d299dd69da62075925e9926b66f8
hunchbck <hunchbck@nate.com>
2024-06-09 12:18:30 +0900

    initial

--- lib/session.ts
+++ lib/session.ts
@@ -1,36 +1,13 @@
-// import { getIronSession } from 'iron-session';
-// import { cookies } from 'next/headers';
-
-// interface SessionContent {
-//   id?: number;
-// }
-
-// export default function getSession() {
-//   return getIronSession<SessionContent>(cookies(), {
-//     cookieName: 'delicious-karrot',
-//     password: process.env.COOKIE_PASSWORD!,
-//   });
-// }
-import { getIronSession, SessionOptions } from 'iron-session';
+import { getIronSession } from 'iron-session';
 import { cookies } from 'next/headers';
 
 interface SessionContent {
   id?: number;
 }
 
-const sessionOptions: SessionOptions = {
-  cookieName: 'delicious-karrot',
-  cookieOptions: {
-    secure: process.env.NODE_ENV === 'production',
-  },
-  password: process.env.COOKIE_PASSWORD as string,
-};
-
-// console.log('Session password:', process.env.COOKIE_PASSWORD);
-
-if (!sessionOptions.password) {
-  throw new Error('COOKIE_PASSWORD 환경 변수가 설정되지 않았습니다.');
-}
-export default async function getSession() {
-  return getIronSession<SessionContent>(cookies(), sessionOptions);
+export default function getSession() {
+  return getIronSession<SessionContent>(cookies(), {
+    cookieName: 'delicious-karrot',
+    password: process.env.COOKIE_PASSWORD!,
+  });
 }
--- middleware.ts
+++ middleware.ts
@@ -1,52 +1,34 @@
-import { getIronSession, SessionOptions } from 'iron-session';
 import { NextRequest, NextResponse } from 'next/server';
 
-interface SessionContent {
-  id?: number;
-}
+import getSession from './lib/session';
 
 interface Routes {
   [key: string]: boolean;
 }
+
 const publicOnlyUrls: Routes = {
   '/': true,
   '/create-account': true,
   '/github/complete': true,
   '/github/start': true,
-  '/kakao/complete': true,
-  '/kakao/start': true,
   '/login': true,
   '/sms': true,
 };
-const sessionOptions: SessionOptions = {
-  cookieName: 'delicious-karrot',
-  cookieOptions: {
-    secure: process.env.NODE_ENV === 'production',
-  },
-  password: process.env.COOKIE_PASSWORD as string,
-};
 
-if (!sessionOptions.password) {
-  throw new Error('COOKIE_PASSWORD 환경 변수가 설정되지 않았습니다.');
-}
 export async function middleware(request: NextRequest) {
-  const cookies = request.cookies;
-  const session = await getIronSession<SessionContent>(cookies, sessionOptions);
+  const session = await getSession();
   const exists = publicOnlyUrls[request.nextUrl.pathname];
-
   if (!session.id) {
     if (!exists) {
       return NextResponse.redirect(new URL('/', request.url));
     }
   } else {
     if (exists) {
-      return NextResponse.redirect(new URL('/products', request.url));
+      return NextResponse.redirect(new URL('/', request.url));
     }
   }
-  return NextResponse.next();
 }
+
 export const config = {
-  matcher: [
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
-  ],
+  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
 };
